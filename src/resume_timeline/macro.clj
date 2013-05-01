8c3d8ac315e27959fab1b418fd1b9b2b510271b2
Steve Shogren <steve.a.shogren@gmail.com>
2013-05-01 17:49:02 -0400

    finializing the last part, just need to remove the parens from the cond args

--- src/resume_timeline/macro.clj
+++ src/resume_timeline/macro.clj
@@ -38,8 +38,8 @@
 (defn defunits-chaining [u units prev]
   (if (some #(= u %) prev)
     (throw (Throwable. (str u " depends on " prev))))
-  (println units)
-  (println "-----")
+  #_(println units)
+  #_(println "-----")
   (let [spec (first (filter #(= u (first %)) units))]
     (if (nil? spec)
       (throw (Throwable. (str "unknown unit " u)))
@@ -54,25 +54,42 @@
 
 (defmacro defunits [quantity base-unit & units]
   `(defmacro ~(symbol (str "unit-of-" quantity))
-     ([valu# un#]
+     [valu# un#]
      `(* ~valu#
-         ~(if (= ~base-unit un#)
-            1
+         ~(if (= un# ~base-unit) 1
             ~@(map (fn [x]
-                    `((~(first x))
-                      ~(defunits-chaining
-                         (first x)
-                         (cons `(~base-unit 1)
-                               (partition 2 units))
-                         nil)))
-                  (partition 2 units)))))))
+                       `(~(first x)
+                         ~(defunits-chaining
+                             (first x)
+                             (cons `(~base-unit 1)
+                                   (partition 2 units))
+                             nil)))
+                     (partition 2 units))
+            #_(map (fn [x]
+                     `(~(first x)
+                       ~(defunits-chaining
+                          (first x)
+                          (cons `(~base-unit 1)
+                                (partition 2 units))
+                          nil)))
+                   (partition 2 units))))))
 
-(macroexpand '(defunits time s
-     m 60
-     h 3600))
+(defmacro test []
+  `(~@(t t)))
+(clojure.pprint/pprint  (macroexpand '(test)))
+
+
+(clojure.pprint/pprint  (macroexpand '(defunits time s
+                   m 60
+                   h 3600)))
+;; transform ->
+;; (if (clojure.core/= un__2631__auto__ s) 1 (m 60) (h 3600)))))))
+;; (if (clojure.core/= un__2631__auto__ s) 1 m 60 h 3600))))))
 (defunits time s
      m 60
      h 3600)
 
 (unit-of-time 4 m)
+
+
 
